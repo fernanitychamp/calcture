c2edf65ce852a2278a1ad5462454f9d57d30460c
Andres Fernando <andres.fernando08@gmail.com>
2018-09-01 15:44:02 -0500

    centrado el div contenedor. agregada funcion para convertir la temperatura y agregado los respectivos handlers para actualizar el valor en el otro input

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,23 +8,58 @@ import TemperatureInput from '../components/TemperatureInput/TemperatureInput';
 class App extends Component {
     constructor(props) {
         super(props)
+        
         this.state = {
-            inputText: ''
+            temperature: '',
+            scale: ''
         }
 
-        this.handleInput = this.handleInput.bind(this)
+        this.handleCelsiusInput = this.handleCelsiusInput.bind(this)
+        this.handleFahrenheitInput = this.handleFahrenheitInput.bind(this)
     }
 
-    handleInput(e) {
-        this.setState({ inputText: e.target.value })
+    convert(scale, temperature) {
+        const input = parseFloat(temperature)
+        let temp = 0
+
+        if (Number.isNaN(input)) {
+            return ''
+        }
+    
+        if (scale === 'c') { // convertir a celsius
+            temp = (input - 32) * 5 / 9
+        }    
+        if (scale === 'f') { // convertir a fahrenheit
+            temp = (input * 9 / 5) + 32
+        } 
+        let rounded = Math.round(temp * 1000 / 1000)
+        return rounded.toString()
     }
+
+    handleCelsiusInput(temperature) {
+        this.setState({ scale: 'c', temperature })
+    }
+
+    handleFahrenheitInput(temperature) {
+        this.setState({ scale: 'f', temperature })
+    }
+
 
     render() {
+        let scale = this.state.scale
+        let temperature = this.state.temperature
+
+        let celsius = scale === 'f' ? this.convert('c', temperature) : temperature
+        let fahrenheit = scale === 'c' ? this.convert('f', temperature) : temperature
+
         return (
             <div className="App">
-                <h2>Welcome to {this.props.appname}</h2>
-                <TemperatureInput value={this.state.inputText} changed={this.handleInput} />
-                <TemperatureStatement temperature={this.state.inputText} />
+                <h2>{this.props.appname}</h2>
+                <div>
+                    <TemperatureStatement temperature={ scale === 'c' ? parseFloat(celsius) : parseFloat(fahrenheit) } />
+                </div>
+                <TemperatureInput scale='c' temperature={celsius} onTemperatureChange={this.handleCelsiusInput} />
+                <TemperatureInput scale='f' temperature={fahrenheit} onTemperatureChange={this.handleFahrenheitInput} />
             </div>
         );
     }
